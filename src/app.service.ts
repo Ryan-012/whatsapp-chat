94cbc8f70f14f0efcf561ab83d225a426750f359
ryan aragao <ryanaragao012@gmail.com>
2023-06-30 12:52:44 -0300

    testes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { VenomInstance } from './venom.instance';
 
+
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(private readonly venomInstance: VenomInstance) {}
 
+  
+
   async onModuleInit() {
+     
     await this.venomInstance.createVenomInstance();
   }
 }
